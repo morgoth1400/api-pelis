0745b09808351057b42fed0215eec7a0a2d71552
morgoth1400 <morgoth1400@gmail.com>
2024-03-01 12:35:28 +0100

    login-working

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const mysql = require("mysql2");
-const bodyParser = require("body-parser");
-const path = require("path");
-const validarId = require("./public/js/middlewares/validarId");
+const express = require("express"),
+  session = require("express-session"),
+  mysql = require("mysql2"),
+  bodyParser = require("body-parser"),
+  path = require("path"),
+  validarId = require("./public/js/middlewares/validarId");
 
 const app = express();
 const PORT = 1972;
@@ -22,6 +23,21 @@ app.use(bodyParser.json());
 // La carpeta de archivos estáticos públicos
 app.use(express.static(path.join(__dirname, "/public")));
 
+app.use(
+  session({
+    secret: "2144-4D32-Wpasd38S-249sffgs-1257412s-d234FgFH",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Authentication and Authorization Middleware FOR ADMIN
+var auth = function (req, res, next) {
+  // if (req.session && userExists(req.session.user) && req.session.admin)
+  if (req.session && req.session.admin) return next();
+  else return res.sendStatus(401);
+}; ///////////////////////////////////////
+
 // Conexión a la base de datos MySQL
 const connection = mysql.createConnection({
   host: "localhost",
@@ -40,6 +56,58 @@ connection.connect((err) => {
 
 ////*****  Rutas de la API *****////
 
+/* RUTAS DE SESIÓN */
+/////////////////////////////////////////PROBAR LO ÚLTIMO QUE COMENTA CHATGPT RESPECTO A LAS RESPUESTAS
+function checkUser(username, password) {
+  return new Promise((resolve, reject) => {
+    const sqlquery = "SELECT pass FROM users WHERE username = ?";
+    connection.query(sqlquery, username, (error, results) => {
+      if (error) {
+        reject(error);
+      } else if (results.length === 0) {
+        resolve(false);
+      } else {
+        //
+        const dbPassword = results[0].pass;
+        resolve(password === dbPassword);
+      }
+    });
+  });
+}
+
+app.post("/login", async function (req, res) {
+  console.log(req.body.username);
+  console.log(req.body.password);
+
+  if (!req.body.username || !req.body.password) {
+    res.send("Login failed");
+    console.log("Login failed (1)");
+  } else {
+    try {
+      const isValidUser = await checkUser(req.body.username, req.body.password);
+      if (isValidUser) {
+        req.session.user = req.body.username;
+        // req.session.admin = true;
+        //TODO: hacer la lógica para verificar si tiene rol de admin
+        // res.send("Login success!");
+        res.redirect("/index");
+      } else {
+        res.send("Login failed");
+        console.log("Login failed (2)");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      res.status(500).send("Error during login");
+    }
+  }
+});
+
+app.get("/logout", function (req, res) {
+  req.session.destroy();
+  res.send("Logout success!");
+  res.redirect("/index");
+});
+
 /* RUTAS WEB O PÚBLICAS*/
 //Redirigir al index
 app.get("/", (req, res) => {
@@ -51,8 +119,8 @@ app.get("/index", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "login.html"));
+app.get("/loginP", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
 });
 
 /*ESTA ES LA FORMA PARA ENVIAR VARIABLES POST AL SERVIDOR 
@@ -61,6 +129,11 @@ app.post("/movie", (req, res) => {
   const movieId = req.body.movieId;
   res.render("movie-template", { movieId: movieId });
 });
+
+// Get content endpoint ////////////////////
+app.get("/content", auth, function (req, res) {
+  res.send("You can only see this after you've logged in.");
+}); ///////////////////////////////////////
 
 /* RUTAS JS */
 //cambiar estos nombres de las URL para que no sean directamente los mismos que los de los archivos js
